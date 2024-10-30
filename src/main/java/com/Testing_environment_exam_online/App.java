a6c48f6ee6cad08cd31b069a639c2e2b02770224
DHuy2003 <duchuy07062003@gmail.com>
2024-10-30 23:00:16 +0700

    rebase main

---
+++ src/main/java/com/Testing_environment_exam_online/App.java
@@ -0,0 +1,13 @@
+package com.Testing_environment_exam_online;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/server/ExamServer.java
@@ -0,0 +1,20 @@
+package server;
+
+import java.io.*;
+import java.net.*;
+
+public class ExamServer {
+    private static final int PORT = 8888;
+
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            System.out.println("Exam Server is running...");
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                new Thread(new ClientHandler(clientSocket)).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
